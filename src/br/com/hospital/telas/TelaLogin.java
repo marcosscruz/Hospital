2dd0e0f44c517185747cee4e618e9e138cb90fc4
Marcela <marcos.v.s.cruz10@gmail.com>
2023-08-05 15:38:08 -0300

    Update TelaLogin.java

--- src/br/com/hospital/telas/TelaLogin.java
+++ src/br/com/hospital/telas/TelaLogin.java
@@ -5,19 +5,20 @@ import java.util.ArrayList;
 import javax.swing.JOptionPane;
 
 public class TelaLogin extends javax.swing.JFrame {
-    
+
     private ArrayList<Usuario> usuarioDataBase;
 
     public TelaLogin() {
         // Inicializa o banco de dados de usuários com um usuário de exemplo
         usuarioDataBase = new ArrayList<>();
         usuarioDataBase.add(new Usuario("login", "senha"));
-        
+
         initComponents();
     }
 
     @SuppressWarnings("unchecked")
-    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    // <editor-fold defaultstate="collapsed" desc="Generated
+    // Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
         logoPequena = new javax.swing.JLabel();
@@ -37,13 +38,15 @@ public class TelaLogin extends javax.swing.JFrame {
         setMinimumSize(new java.awt.Dimension(500, 250));
         setResizable(false);
 
-        logoPequena.setIcon(new javax.swing.ImageIcon(getClass().getResource("/br/com/hospital/imagens/logo_login.png"))); // NOI18N
+        logoPequena
+                .setIcon(new javax.swing.ImageIcon(getClass().getResource("/br/com/hospital/imagens/logo_login.png"))); // NOI18N
 
         textLogin.setText("Login:");
 
         textSenha.setText("Senha:");
 
-        loginButton.setIcon(new javax.swing.ImageIcon(getClass().getResource("/br/com/hospital/imagens/entrar_sistema.png"))); // NOI18N
+        loginButton.setIcon(
+                new javax.swing.ImageIcon(getClass().getResource("/br/com/hospital/imagens/entrar_sistema.png"))); // NOI18N
         loginButton.setText("Entrar");
         loginButton.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         loginButton.addActionListener(new java.awt.event.ActionListener() {
@@ -52,7 +55,8 @@ public class TelaLogin extends javax.swing.JFrame {
             }
         });
 
-        sairButton.setIcon(new javax.swing.ImageIcon(getClass().getResource("/br/com/hospital/imagens/sair_sistema.png"))); // NOI18N
+        sairButton.setIcon(
+                new javax.swing.ImageIcon(getClass().getResource("/br/com/hospital/imagens/sair_sistema.png"))); // NOI18N
         sairButton.setText("Sair");
         sairButton.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         sairButton.addActionListener(new java.awt.event.ActionListener() {
@@ -64,82 +68,86 @@ public class TelaLogin extends javax.swing.JFrame {
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(54, 54, 54)
-                .addComponent(logoPequena)
-                .addGap(59, 59, 59)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(textLogin)
-                    .addComponent(usuarioLogin)
-                    .addComponent(textSenha)
-                    .addComponent(usuarioSenha)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(loginButton, javax.swing.GroupLayout.PREFERRED_SIZE, 82, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(sairButton, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(119, Short.MAX_VALUE))
-        );
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(layout.createSequentialGroup()
+                                .addGap(54, 54, 54)
+                                .addComponent(logoPequena)
+                                .addGap(59, 59, 59)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                        .addComponent(textLogin)
+                                        .addComponent(usuarioLogin)
+                                        .addComponent(textSenha)
+                                        .addComponent(usuarioSenha)
+                                        .addGroup(layout.createSequentialGroup()
+                                                .addComponent(loginButton, javax.swing.GroupLayout.PREFERRED_SIZE, 82,
+                                                        javax.swing.GroupLayout.PREFERRED_SIZE)
+                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                                .addComponent(sairButton, javax.swing.GroupLayout.PREFERRED_SIZE, 80,
+                                                        javax.swing.GroupLayout.PREFERRED_SIZE)))
+                                .addContainerGap(51, Short.MAX_VALUE)));
         layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(60, 60, 60)
-                        .addComponent(logoPequena))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(30, 30, 30)
-                        .addComponent(textLogin)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(usuarioLogin, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(18, 18, 18)
-                        .addComponent(textSenha)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(usuarioSenha, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(18, 18, 18)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(loginButton)
-                            .addComponent(sairButton))))
-                .addContainerGap(81, Short.MAX_VALUE))
-        );
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                        .addGroup(layout.createSequentialGroup()
+                                                .addGap(60, 60, 60)
+                                                .addComponent(logoPequena))
+                                        .addGroup(layout.createSequentialGroup()
+                                                .addGap(30, 30, 30)
+                                                .addComponent(textLogin)
+                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                                .addComponent(usuarioLogin, javax.swing.GroupLayout.PREFERRED_SIZE,
+                                                        javax.swing.GroupLayout.DEFAULT_SIZE,
+                                                        javax.swing.GroupLayout.PREFERRED_SIZE)
+                                                .addGap(18, 18, 18)
+                                                .addComponent(textSenha)
+                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                                .addComponent(usuarioSenha, javax.swing.GroupLayout.PREFERRED_SIZE,
+                                                        javax.swing.GroupLayout.DEFAULT_SIZE,
+                                                        javax.swing.GroupLayout.PREFERRED_SIZE)
+                                                .addGap(18, 18, 18)
+                                                .addGroup(layout
+                                                        .createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                                        .addComponent(loginButton)
+                                                        .addComponent(sairButton))))
+                                .addContainerGap(81, Short.MAX_VALUE)));
 
         pack();
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
-    private void loginButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_loginButtonActionPerformed
+    private void loginButtonActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_loginButtonActionPerformed
         // TODO add your handling code here:
         String nomeUser = usuarioLogin.getText();
         char[] senhaUser = usuarioSenha.getPassword();
-        
-        if(nomeUser.isEmpty() || senhaUser.length == 0) {
+
+        if (nomeUser.isEmpty() || senhaUser.length == 0) {
             JOptionPane.showMessageDialog(TelaLogin.this, "Preencha todos os campos!");
         }
-        
+
         boolean autiticado = false;
-        for(Usuario usuario : usuarioDataBase){
-            if(usuario.getNomeUsuario().equals(nomeUser) && usuario.getSenhaUsuario().equals(new String(senhaUser))){
+        for (Usuario usuario : usuarioDataBase) {
+            if (usuario.getNomeUsuario().equals(nomeUser) && usuario.getSenhaUsuario().equals(new String(senhaUser))) {
                 autiticado = true;
                 break;
             }
         }
-        
-        if(autiticado){
+
+        if (autiticado) {
             TelaPrincipal principal = new TelaPrincipal();
             principal.setVisible(true);
-            
+
             setVisible(false);
             dispose();
         } else {
             JOptionPane.showMessageDialog(TelaLogin.this, "Usuário ou Senha inconrretos!");
         }
-    }//GEN-LAST:event_loginButtonActionPerformed
+    }// GEN-LAST:event_loginButtonActionPerformed
 
-    private void sairButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_sairButtonActionPerformed
+    private void sairButtonActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_sairButtonActionPerformed
         // TODO add your handling code here:
         System.exit(0);
-    }//GEN-LAST:event_sairButtonActionPerformed
-
+    }// GEN-LAST:event_sairButtonActionPerformed
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton loginButton;
